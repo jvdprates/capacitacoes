d19963611eb5cdd08a3f86fff2bc8ccf97478fa1
João Prates <jvdprates@hotmail.com>
2020-05-06 19:57:22 -0300

    Resetando e preparando a capacitação

---
+++ capacitacao_git/frontend/src/pages/Grupo2/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './styles.css';
+
+/*
+Passos e Orientações :
+- Uma vez copiado o diretório crie um branch para seu grupo e trabalhe sempre nele.
+- Rode npm install e começe a trabalhar na página do seu grupo dentro de /pages.
+- Cada grupo deverá criar uma página em um intervalo de tempo estipulado na capacitação (prazo sujeito a mudança).
+- O conteúdo e formato desta página está completamente à critério do grupo.
+- Use esta oportunidade para mostrar os conhecimentos adquiridos durante as últimas capacitações.
+- Se quiser, crie pastas para seus componentes dentro de /components.
+- Se você manjar muito e estiver animado crie pastas para conecções em /connection com o Axios.
+- Ao final do tempo estipulado pare de trabalhar e dê o commit.
+- Todos os branchs de todos os grupos serão unidos ao master e o resultado final será mostrado em seguida.
+
+Regras da dinâmica:
+- Vocês não poderão instalar nenhuma dependencia extra no projeto, trabalhem com o que já tem instalado.
+- Use apenas styles.css da pasta do seu grupo para estilos.
+- Tenta levar a sério pfvr, quero ver coisa bonita, coisa arrumada, coisa charmosa.
+- Não mecha em routes.js, App.js nem com o index.js global e pastas de outros grupos.
+- Divirta-se?
+- É isso.
+- Se quiser pode apagar esse comentário (depois de ler tudo é claro).
+*/
+
+export default function Grupo2(){
+
+  return (
+    <div>
+        <h1>Grupo 2</h1>
+    </div>
+  );
+}
