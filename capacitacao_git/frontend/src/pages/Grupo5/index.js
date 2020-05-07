fd2409fdd394722a118a71f9d9e40714c55aaad9
Gabriel Andrade <gabrielolandrade@hotmail.com.br>
2020-05-07 16:31:06 -0300

    Calculadora do grupo 5 #nazaré

--- capacitacao_git/frontend/src/pages/Grupo5/index.js
+++ capacitacao_git/frontend/src/pages/Grupo5/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 /*
@@ -21,9 +21,51 @@ Regras da dinâmica:
 
 export default function Grupo5(){
 
+  const [num1, setNum1]=useState(0)
+  const [num2, setNum2]=useState(0)
+
+  const [res, setRes]=useState(0)
+
+  console.log(num1)
+  console.log(num2)
+
+  function somar(num1, num2){
+    setRes(num1+num2)
+  }
+  
+  function subtrair(num1, num2){
+    setRes(num1-num2)
+  }
+
+  
+  function multiplicar(num1, num2){
+    setRes(num1*num2)
+  }
+  
+  function dividir(num1, num2){
+    setRes((num1/num2).toFixed(2))
+  }
+  //parseInt(e.target.value)
   return (
-    <div>
-        <h1>Grupo 5</h1>
+    <div style={{textAlign:"center"}}>
+       <h1>Calculadora Grupo 5</h1>
+        <input type='text' placeholder='Número 1' onChange={(e)=>{setNum1(parseInt(e.target.value,10))}}/><br></br>
+        <input type="text" placeholder='Número 2' onChange={(e)=>{setNum2(parseInt(e.target.value,10))}}/><br></br>
+          <br></br>
+        <button onClick={()=>{somar(num1,num2)}}>Somar</button>
+        <button onClick={()=>{subtrair(num1,num2)}}>Subtrair</button>
+        <button onClick={()=>{multiplicar(num1,num2)}}>Multiplicar</button>
+        <button onClick={()=>{dividir(num1,num2)}}>Dividir</button>
+        <hr></hr>
+   <h1>Resultado:{res}</h1>
+   <img src='https://cdn.dicionariopopular.com/imagens/nazareconfusamatematica.gif'/>
     </div>
   );
 }
+
+/* function calculadora(){
+
+  return(){
+    
+  }
+} */
