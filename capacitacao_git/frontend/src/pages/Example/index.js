e68c008aedc951a9468ece613394b253968daf5b
João Prates <jvdprates@hotmail.com>
2020-05-06 21:04:05 -0300

    Trabalhando na pagina de exemplo

--- capacitacao_git/frontend/src/pages/Example/index.js
+++ capacitacao_git/frontend/src/pages/Example/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Example(){
+
+export default function Example() {
 
   return (
-    <div>
+    <div className="fullContainer">
+      <div className="d-flex justify-content-center">
+        <h1 className="mainTitle">Página de exemplo</h1>
+      </div>
+      <div className="centralBox align-itens-center">
+        <button>A</button>
+      </div>
     </div>
   );
 }
