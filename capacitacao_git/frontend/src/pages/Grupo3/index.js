6026eb2365e13d47948b42e9e388bfffa6388a5d
vitor <vitor.holler@outlook.pt>
2020-05-08 19:03:13 -0300

    Grupo 03

--- capacitacao_git/frontend/src/pages/Grupo3/index.js
+++ capacitacao_git/frontend/src/pages/Grupo3/index.js
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { Button, InputGroup, FormControl } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-/*
-Passos e Orientações :
-- Uma vez copiado o diretório crie um branch para seu grupo e trabalhe sempre nele.
-- Rode npm install e começe a trabalhar na página do seu grupo dentro de /pages.
-- Cada grupo deverá criar uma página em um intervalo de tempo estipulado na capacitação (prazo sujeito a mudança).
-- Use esta oportunidade para mostrar os conhecimentos adquiridos durante as últimas capacitações.
-- Ao final do tempo estipulado pare de trabalhar e dê o commit.
-- Todos os branchs de todos os grupos serão unidos ao master e o resultado final será mostrado em seguida.
-- Se precisarem de referência para criação de uma página, deixei a página localizada em /pages/Example para vocês consultarem.
+export default function Grupo3() {
+    const [number1, setNumber1] = useState()
+    const [number2, setNumber2] = useState()
+    const [result, setResult] = useState(0)
 
-Regras da dinâmica:
-- Vocês não poderão instalar nenhuma dependencia extra no projeto, trabalhem com o que já tem instalado.
-- Use apenas styles.css da pasta do seu grupo para estilos.
-- Nesses estilos não sobrescreva componentes padrão do html, crie classes css com nome no formato -> g*_nomeClasse / para * = número do seu grupo.
-- Não faça nenhuma alteração em routes.js, App.js nem com o index.js global e pastas de outros grupos.
-- Se quiser pode apagar esse comentário (depois de ler tudo é claro).
-*/
 
-export default function Grupo3(){
+    function sum(num1, num2) {
+        return -(-num1 - num2)
+    }
 
-  return (
-    <div>
-        <h1>Grupo 3</h1>
-    </div>
-  );
+    function diff(num1, num2) {
+        return num1 - num2
+    }
+
+    function times(num1, num2) {
+        return num1 * num2
+    }
+
+    function division(num1, num2) {
+        return num1 / num2
+    }
+
+    function reset() {
+        setNumber1(0)
+        setNumber2(0)
+        setResult(0)
+    }
+
+    return (
+        <div className='calculadora'>
+            <InputGroup className='texto d-flex mt-2 ml-5 '>
+                <InputGroup.Prepend>
+                    <InputGroup.Text>Coloque os números para realizar as operações</InputGroup.Text>
+                    <FormControl className='input' value={number1} onChange={e => setNumber1(e.target.value)} />
+                    <FormControl className='input' value={number2} onChange={e => setNumber2(e.target.value)} />
+                </InputGroup.Prepend>
+            </InputGroup>
+            {console.log(number1, number2)}
+            <div className='button d-flex flex-column mt-4'>
+                <div>
+                    <Button className='mais' value={result} onClick={() => setResult(sum(number1, number2))}>+</Button>
+                    <Button className='diff' value={result} onClick={() => setResult(diff(number1, number2))}>-</Button>
+                </div>
+                <div>
+                    <Button className='vezes' value={result} onClick={() => setResult(times(number1, number2))}>*</Button>
+                    <Button className='' value={result} onClick={() => setResult(division(number1, number2))}>/</Button>
+                </div>
+                <div><Button className='reset' value={result} onClick={() => reset()}>Reset</Button>
+                </div>
+            </div>
+            <div className='resultado'>
+                <h1>Resultado: {result}</h1>
+            </div>
+        </div>
+    )
 }
